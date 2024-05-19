3e2c54804f5048bb79fc3e7f5ed0cb8ca6038505
Osarugue Enehizena <nehirugue@gmail.com>
2024-05-19 23:59:00 +0100

    Adding Background Image

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "../screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function Index() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("../assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -13,6 +20,10 @@ export default function Index() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f",
+    // backgroundColor: "#ddb52f",
+  },
+
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
